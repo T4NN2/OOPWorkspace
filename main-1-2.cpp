70bafa46c90ca5aaa9efe58c3f1c0ff01e1a2d1a
T4NN2 <tranthaian2006@gmail.com>
2025-03-05 21:04:39 +1030

    another minor change

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,6 +4,6 @@ extern double array_mean(int array[], int n);
 
 int main() {
     int array[3] = {2, 4, 6};
-    std::cout <<"The mean of all elements in the array is: " << array_mean(array, 3) std::endl;
+    std::cout <<"The mean of all elements in the array is: " << array_mean(array, 3) << std::endl;
     return 0;
 }
