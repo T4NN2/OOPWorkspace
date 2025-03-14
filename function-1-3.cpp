f77501369687b853b8730e0a2fcba2bd3a574ba6
T4NN2 <tranthaian2006@gmail.com>
2025-03-14 19:47:54 +1030

    -1-3

---
+++ function-1-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void count_digits(int array[4][4]) {
+    int count[10] = {0}; 
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            int num = array[i][j];
+            if (num >= 0 && num <= 9) {
+                count[num]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << i << ":" << count[i] << ";";
+    }
+}
---
+++ main-1-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+extern void count_digits(int array[4][4]);
+
+int main() {
+    int array[4][4] = {
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0}
+    };
+
+    count_digits(array);
+}
