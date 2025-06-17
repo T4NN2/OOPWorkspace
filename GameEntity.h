eabf9812b191e2335470c96c60e23fe717815472
Tann <tranthaian2006@gmail.com>
2025-06-17 23:22:27 +0930

    sdfg

--- GameEntity.h
+++ GameEntity.h
@@ -1,19 +1,19 @@
 #ifndef GAMEENTITY_H
 #define GAMEENTITY_H
 
-#include <tuple>
+#include <utility>
 
 enum GameEntityType { ExplosionType, MineType, NoneType, ShipType };
 
 class GameEntity {
 protected:
-    std::tuple<int, int> position;
+    std::pair<int, int> position;
     GameEntityType type;
 
 public:
     GameEntity(int x, int y, GameEntityType t) : position(x, y), type(t) {}
 
-    std::tuple<int, int> getPos() const { return position; }
+    std::pair<int, int> getPos() const { return position; }
     GameEntityType getType() const { return type; }
 
     virtual ~GameEntity() = default;
